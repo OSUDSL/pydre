0ddc2a25a779299708fff95bd16ca0a3bd812be5
Thomas Kerwin <kerwin.6@osu.edu>
2016-03-04 08:33:35 -0500

    Fix formatting for steeringEntropy(), mostly PEP8 compliant

--- pydre/metrics.py
+++ pydre/metrics.py
@@ -3,10 +3,11 @@
 
 import pandas
 import pydre.core
-import np
+import numpy
 import math
 
 metricsList = {}
+
 
 # metrics defined here take a list of DriveData objects and return a single floating point value
 def meanVelocity(regions, cutoff=0):
@@ -16,48 +17,51 @@ def meanVelocity(regions, cutoff=0):
 		total_vel = total_vel.append(data[data.Velocity >= cutoff].Velocity)
 	return total_vel.mean()
 
-def steeringEntropy(regions,cutoff=0):
-    out = []
-    for i in range(0, len(regions)):
-        df = pandas.DataFrame(regions[i].data)
-        df = pandas.DataFrame(df, columns=("SimTime","Steer")) #drop other columns
-        df = pandas.DataFrame.drop_duplicates(df.dropna(axis = [0,1], how='any')) #remove nans and drop duplicates
 
-        #resample data
-        minTime = df.SimTime.values.min()
-        maxTime = df.SimTime.values.max()
-        nsteps = math.floor((maxTime - minTime) / 0.0167)  #divide into 0.0167s increments
-        regTime = numpy.linspace(minTime, maxTime, num=nsteps)
-        rsSteer = numpy.interp(regTime, df.SimTime, df.Steer)
-        resampdf = numpy.column_stack((regTime, rsSteer))
-        resampdf = pandas.DataFrame(resampdf, columns=("simTime","rsSteerAngle")) #df
+def steeringEntropy(regions, cutoff=0):
+	out = []
+	for i in range(0, len(regions)):
+		df = pandas.DataFrame(regions[i].data)
+		df = pandas.DataFrame(df, columns=("SimTime", "Steer"))  # drop other columns
+		df = pandas.DataFrame.drop_duplicates(df.dropna(axis=[0, 1], how='any'))  # remove nans and drop duplicates
 
-        #calculate predicted angle
-        pAngle = (2.5 * df.Steer.values[3:,]) - (2 * df.Steer.values[2:-1,]) - (0.5 * df.Steer.values[1:-2,]);
+		# resample data
+		minTime = df.SimTime.values.min()
+		maxTime = df.SimTime.values.max()
+		nsteps = math.floor((maxTime - minTime) / 0.0167)  # divide into 0.0167s increments
+		regTime = numpy.linspace(minTime, maxTime, num=nsteps)
+		rsSteer = numpy.interp(regTime, df.SimTime, df.Steer)
+		resampdf = numpy.column_stack((regTime, rsSteer))
+		resampdf = pandas.DataFrame(resampdf, columns=("simTime", "rsSteerAngle"))
 
-        #calculate error
-        error = df.Steer.values[3:,] - pAngle
-        out.append(error)
+		# calculate predicted angle
+		pAngle = (2.5 * df.Steer.values[3:, ]) - (2 * df.Steer.values[2:-1, ]) - (0.5 * df.Steer.values[1:-2, ])
 
-    #concatenate out (list of np objects) into a single list
-    error = numpy.stack(out)
-    #90th percentile of error
-    alpha = numpy.nanpercentile(error,90)
+		# calculate error
+		error = df.Steer.values[3:, ] - pAngle
+		out.append(error)
 
-    #divide into 9 bins with edges: -5a,-2.5a,-a,a,2.5,5a
-    binnedError = numpy.histogram(error, bins= [-10 * abs(alpha), -5 * abs(alpha), -2.5 * abs(alpha), -abs(alpha), -0.5 * abs(alpha), 0.5 * abs(alpha), abs(alpha), 2.5 * abs(alpha), 5 * abs(alpha), 10 * abs(alpha)])
+	# concatenate out (list of np objects) into a single list
+	error = numpy.stack(out)
+	# 90th percentile of error
+	alpha = numpy.nanpercentile(error, 90)
 
-    #calculate H
-    binnedArr = numpy.asarray(binnedError[0])
-    binnedArr = binnedArr.astype(float)
+	# divide into 9 bins with edges: -5a,-2.5a,-a,a,2.5,5a
+	binnedError = numpy.histogram(error, bins=[-10 * abs(alpha), -5 * abs(alpha), -2.5 * abs(alpha),
+		-abs(alpha), -0.5 * abs(alpha), 0.5 * abs(alpha), abs(alpha),
+		2.5 * abs(alpha), 5 * abs(alpha), 10 * abs(alpha)])
 
-    #check out calc of p
-    p = numpy.divide(binnedArr,numpy.sum(binnedArr))
-    Hp = numpy.multiply(numpy.multiply(-1,p),(numpy.log(p)/numpy.log(9)))
-    Hp = Hp[~numpy.isnan(Hp)]
-    Hp = numpy.sum(Hp)
+	# calculate H
+	binnedArr = numpy.asarray(binnedError[0])
+	binnedArr = binnedArr.astype(float)
 
-    return Hp
+	# check out calc of p
+	p = numpy.divide(binnedArr, numpy.sum(binnedArr))
+	Hp = numpy.multiply(numpy.multiply(-1, p), (numpy.log(p)/numpy.log(9)))
+	Hp = Hp[~numpy.isnan(Hp)]
+	Hp = numpy.sum(Hp)
+
+	return Hp
 
 metricsList['meanVelocity'] = meanVelocity
 metricsList['steeringEntropy'] = steeringEntropy
