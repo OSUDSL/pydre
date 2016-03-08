00e873d89370d1fb3be6f779d3f3293d561f9aa1
Thomas Kerwin <kerwin.6@osu.edu>
2016-03-08 12:14:16 -0500

    Use example file in the repo for the smoketest

--- tests/smoketest.py
+++ tests/smoketest.py
@@ -12,7 +12,7 @@ import glob
 import logging
 logging.basicConfig(level=logging.INFO)
 
-p = pydre.project.Project("docs/test_pf.json")
+p = pydre.project.Project("docs/example_projectfile.json")
 
 # put some data files in tests/testdata/ to test it out
 filelist = glob.glob(os.path.join(os.path.dirname(__file__), 'testdata/CWS2_Sub_16_*.dat'))
