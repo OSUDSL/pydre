903ee98e5ef5ea82842800d726878ae41d820801
Kevin Smearsoll <smearsoll.4@osu.edu>
2016-03-24 16:13:41 -0400

    Fixed minor bugs in rois and steering entropy metric. Setup to run Tyler's data

--- pydre/metrics.py
+++ pydre/metrics.py
@@ -20,44 +20,48 @@ def meanVelocity(drivedata: pydre.core.DriveData, cutoff=0):
 def steeringEntropy(drivedata: pydre.core.DriveData, cutoff=0):
 	out = []
 	for d in drivedata.data:
-		df = pandas.DataFrame(d, columns=("SimTime", "Steer"))  # drop other columns
-		df = pandas.DataFrame.drop_duplicates(df.dropna(axis=[0, 1], how='any'))  # remove nans and drop duplicates
+		if(len(d) > 0):
+			df = pandas.DataFrame(d, columns=("SimTime", "Steer"))  # drop other columns
+			df = pandas.DataFrame.drop_duplicates(df.dropna(axis=[0, 1], how='any'))  # remove nans and drop duplicates
 
-		# resample data
-		minTime = df.SimTime.values.min()
-		maxTime = df.SimTime.values.max()
-		nsteps = math.floor((maxTime - minTime) / 0.0167)  # divide into 0.0167s increments
-		regTime = numpy.linspace(minTime, maxTime, num=nsteps)
-		rsSteer = numpy.interp(regTime, df.SimTime, df.Steer)
-		resampdf = numpy.column_stack((regTime, rsSteer))
-		resampdf = pandas.DataFrame(resampdf, columns=("simTime", "rsSteerAngle"))
+			# resample data
+			minTime = df.SimTime.values.min()
+			maxTime = df.SimTime.values.max()
+			nsteps = math.floor((maxTime - minTime) / 0.0167)  # divide into 0.0167s increments
+			regTime = numpy.linspace(minTime, maxTime, num=nsteps)
+			rsSteer = numpy.interp(regTime, df.SimTime, df.Steer)
+			resampdf = numpy.column_stack((regTime, rsSteer))
+			resampdf = pandas.DataFrame(resampdf, columns=("simTime", "rsSteerAngle"))
 
-		# calculate predicted angle
-		pAngle = (2.5 * df.Steer.values[3:, ]) - (2 * df.Steer.values[2:-1, ]) - (0.5 * df.Steer.values[1:-2, ])
+			# calculate predicted angle
+			pAngle = (2.5 * df.Steer.values[3:, ]) - (2 * df.Steer.values[2:-1, ]) - (0.5 * df.Steer.values[1:-2, ])
 
-		# calculate error
-		error = df.Steer.values[3:, ] - pAngle
-		out.append(error)
+			# calculate error
+			error = df.Steer.values[3:, ] - pAngle
+			out.append(error)
 
-	# concatenate out (list of np objects) into a single list
-	error = numpy.concatenate(out)
-	# 90th percentile of error
-	alpha = numpy.nanpercentile(error, 90)
+	if len(out) > 0:
+		# concatenate out (list of np objects) into a single list
+		error = numpy.concatenate(out)
+		# 90th percentile of error
+		alpha = numpy.nanpercentile(error, 90)
 
-	# divide into 9 bins with edges: -5a,-2.5a,-a,a,2.5,5a
-	binnedError = numpy.histogram(error, bins=[-10 * abs(alpha), -5 * abs(alpha), -2.5 * abs(alpha),
-		-abs(alpha), -0.5 * abs(alpha), 0.5 * abs(alpha), abs(alpha),
-		2.5 * abs(alpha), 5 * abs(alpha), 10 * abs(alpha)])
+		# divide into 9 bins with edges: -5a,-2.5a,-a,a,2.5,5a
+		binnedError = numpy.histogram(error, bins=[-10 * abs(alpha), -5 * abs(alpha), -2.5 * abs(alpha),
+			-abs(alpha), -0.5 * abs(alpha), 0.5 * abs(alpha), abs(alpha),
+			2.5 * abs(alpha), 5 * abs(alpha), 10 * abs(alpha)])
 
-	# calculate H
-	binnedArr = numpy.asarray(binnedError[0])
-	binnedArr = binnedArr.astype(float)
+		# calculate H
+		binnedArr = numpy.asarray(binnedError[0])
+		binnedArr = binnedArr.astype(float)
 
-	# check out calc of p
-	p = numpy.divide(binnedArr, numpy.sum(binnedArr))
-	Hp = numpy.multiply(numpy.multiply(-1, p), (numpy.log(p)/numpy.log(9)))
-	Hp = Hp[~numpy.isnan(Hp)]
-	Hp = numpy.sum(Hp)
+		# check out calc of p
+		p = numpy.divide(binnedArr, numpy.sum(binnedArr))
+		Hp = numpy.multiply(numpy.multiply(-1, p), (numpy.log(p)/numpy.log(9)))
+		Hp = Hp[~numpy.isnan(Hp)]
+		Hp = numpy.sum(Hp)
+	else:
+		Hp = numpy.nan
 
 	return Hp
 
