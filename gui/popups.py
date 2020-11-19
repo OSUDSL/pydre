9d809fdcffb1bfb19c682c779814b93390440ce7
BuildTools <fouts.57@osu.edu>
2020-11-19 12:29:20 -0500

    Added project file viewing capability

--- gui/popups.py
+++ gui/popups.py
@@ -15,19 +15,21 @@ class ProjectFilePopup(Popup):
     configuring the given project JSON file.
     """
 
-    def __init__(self, project_file, *args, **kwargs):
+    def __init__(self, project_file):
         super().__init__(Ui_Form, "images/icon.png", "Project Editor")
 
-        # self.project_file = project_file
-        #
-        # self.layout = QVBoxLayout()
-        #
-        # self.json_tree = QTreeWidget()
-        # self.json_tree.setHeaderLabels(['Project parameters'])
-        # self._build_tree()
-        #
-        # self.layout.addWidget(self.json_tree)
-        # self.setLayout(self.layout)
+        # Class parameters
+        self.project_file = project_file
+
+        # Class variables
+        self.param_types = ['roi', 'metric']
+
+        # Button callbacks
+        self.ui.cancel_btn.clicked.connect(self.close)
+        self.ui.save_btn.clicked.connect(self._save)
+        self.ui.finish_btn.clicked.connect(self._finish)
+
+        self._build_tree()
 
     def _build_tree(self):
         """
@@ -35,13 +37,30 @@ class ProjectFilePopup(Popup):
         """
 
         pfile_contents = json.load(self.project_file)
+
+        tree_count = 0
         for i in pfile_contents:
-            parent = QTreeWidgetItem(self.json_tree, [i])
-            metric = 1
+            tree = QTreeWidgetItem(self.ui.pfile_tree, [i])
+            leaf_count = 1
             for j in pfile_contents[i]:
-                branch = QTreeWidgetItem(parent, ['{0} {1}'.format(i, metric)])
-                # child = QTreeWidgetItem(parent, ['{0}: {1}'.format(k, j[k]) for k in j])
+                branch = QTreeWidgetItem(tree, [
+                    '{0} {1}'.format(self.param_types[tree_count], leaf_count)])
                 for k in j:
-                    child = QTreeWidgetItem(branch,
-                                            ['{0}: {1}'.format(k, j[k])])
-                metric += 1
+                    QTreeWidgetItem(branch, ['{0}: {1}'.format(k, j[k])])
+                leaf_count += 1
+            tree_count += 1
+
+    def _save(self):
+        """
+        FIXME
+        """
+
+        print("DEBUG: Project file saved :)")
+
+    def _finish(self):
+        """
+        FIXME
+        """
+
+        self._save()
+        self.close()
