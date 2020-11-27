eeccd3513ce8c7307052145c84ad460e91e08f72
BuildTools <fouts.57@osu.edu>
2020-11-26 22:18:20 -0500

    Created custom ProjectTree widget

--- gui/customs.py
+++ gui/customs.py
@@ -16,33 +16,49 @@ class ProjectTree(QTreeWidget):
     TODO
     """
 
-    def __init__(self, headers, animated=False, *args, **kwargs):
+    def __init__(self, headers=None, animated=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.setHeaderLabels(headers)
-        self.setAnimated(animated)
-
+        # Config variables
         self.param_types = dict(config.items("parameters"))
 
+        # Class variables
+        if headers is None:
+            self.headers = ["Project parameters"]
+        else:
+            self.headers = headers
+
+        # Widget configurations
+        self.setHeaderLabels(self.headers)
+        self.setAnimated(animated)
+
     @staticmethod
-    def _fill_leaves(branch, parameters):
+    def _fill_leaves(branch, attributes):
         """TODO"""
 
-        for param in parameters:
-            text = ["{0}: {1}".format(param, parameters[param])]
+        # Generate leaves with the give attributes list
+        for i in attributes:
+            text = ["{0}: {1}".format(i, attributes[i])]
             QTreeWidgetItem(branch, text)
 
-    def _fill_branches(self, tree, parameters):
+    def _fill_branches(self, tree, contents, parameter):
         """TODO"""
 
-        for index, type_ in parameters:
-            text = ["{0} {1}".format()]
+        # Generate branches for the specified parameter
+        for index, i in enumerate(contents[parameter]):
+            text = ["{0} {1}".format(self.param_types[parameter], index + 1)]
+            branch = QTreeWidgetItem(tree, text)
+            self._fill_leaves(branch, i)
 
-    def build(self, path):
+    def build_from_file(self, path):
         """
         TODO
         """
 
+        # Load project file contents
+        contents = load(open(path))
 
-
-
+        # Generate tree for each parameter type
+        for i in contents:
+            tree = QTreeWidgetItem(self, [i])
+            self._fill_branches(tree, contents, i)
