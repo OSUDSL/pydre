bce5a93b5f1a8cd1a0259d9162279f087d2f6632
BuildTools <fouts.57@osu.edu>
2020-11-27 22:48:34 -0500

    Added page for running pydre

--- gui/ui_files/ui_mainwindow.py
+++ gui/ui_files/ui_mainwindow.py
@@ -17,6 +17,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
+        MainWindow.setEnabled(True)
         MainWindow.resize(500, 268)
         MainWindow.setMinimumSize(QSize(0, 0))
         MainWindow.setMaximumSize(QSize(16777215, 16777215))
@@ -29,6 +30,8 @@ class Ui_MainWindow(object):
         self.new_action.setObjectName(u"new_action")
         self.open_action = QAction(MainWindow)
         self.open_action.setObjectName(u"open_action")
+        self.run_action = QAction(MainWindow)
+        self.run_action.setObjectName(u"run_action")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
@@ -126,30 +129,30 @@ class Ui_MainWindow(object):
         self.page_3.setObjectName(u"page_3")
         self.verticalLayout_2 = QVBoxLayout(self.page_3)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.groupBox = QGroupBox(self.page_3)
-        self.groupBox.setObjectName(u"groupBox")
-        self.verticalLayout_8 = QVBoxLayout(self.groupBox)
+        self.run_box = QGroupBox(self.page_3)
+        self.run_box.setObjectName(u"run_box")
+        self.verticalLayout_8 = QVBoxLayout(self.run_box)
         self.verticalLayout_8.setObjectName(u"verticalLayout_8")
-        self.label = QLabel(self.groupBox)
+        self.label = QLabel(self.run_box)
         self.label.setObjectName(u"label")
 
         self.verticalLayout_8.addWidget(self.label)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.listWidget = QListWidget(self.groupBox)
+        self.listWidget = QListWidget(self.run_box)
         self.listWidget.setObjectName(u"listWidget")
 
         self.horizontalLayout.addWidget(self.listWidget)
 
         self.verticalLayout_5 = QVBoxLayout()
         self.verticalLayout_5.setObjectName(u"verticalLayout_5")
-        self.pushButton = QPushButton(self.groupBox)
+        self.pushButton = QPushButton(self.run_box)
         self.pushButton.setObjectName(u"pushButton")
 
         self.verticalLayout_5.addWidget(self.pushButton)
 
-        self.pushButton_2 = QPushButton(self.groupBox)
+        self.pushButton_2 = QPushButton(self.run_box)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
         self.verticalLayout_5.addWidget(self.pushButton_2)
@@ -164,25 +167,25 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_8.addLayout(self.horizontalLayout)
 
-        self.label_3 = QLabel(self.groupBox)
+        self.label_3 = QLabel(self.run_box)
         self.label_3.setObjectName(u"label_3")
 
         self.verticalLayout_8.addWidget(self.label_3)
 
-        self.lineEdit = QLineEdit(self.groupBox)
+        self.lineEdit = QLineEdit(self.run_box)
         self.lineEdit.setObjectName(u"lineEdit")
 
         self.verticalLayout_8.addWidget(self.lineEdit)
 
 
-        self.verticalLayout_2.addWidget(self.groupBox)
+        self.verticalLayout_2.addWidget(self.run_box)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.pushButton_3 = QPushButton(self.page_3)
-        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.cancel_btn = QPushButton(self.page_3)
+        self.cancel_btn.setObjectName(u"cancel_btn")
 
-        self.horizontalLayout_3.addWidget(self.pushButton_3)
+        self.horizontalLayout_3.addWidget(self.cancel_btn)
 
         self.pushButton_4 = QPushButton(self.page_3)
         self.pushButton_4.setObjectName(u"pushButton_4")
@@ -198,26 +201,27 @@ class Ui_MainWindow(object):
         self.verticalLayout.addWidget(self.page_stack)
 
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menuBar = QMenuBar(MainWindow)
-        self.menuBar.setObjectName(u"menuBar")
-        self.menuBar.setGeometry(QRect(0, 0, 500, 21))
-        self.menuFile = QMenu(self.menuBar)
-        self.menuFile.setObjectName(u"menuFile")
-        self.menuEdit = QMenu(self.menuBar)
-        self.menuEdit.setObjectName(u"menuEdit")
-        self.menuView = QMenu(self.menuBar)
-        self.menuView.setObjectName(u"menuView")
-        self.menuRun = QMenu(self.menuBar)
-        self.menuRun.setObjectName(u"menuRun")
-        self.menuRun.setEnabled(False)
-        MainWindow.setMenuBar(self.menuBar)
+        self.menu_bar = QMenuBar(MainWindow)
+        self.menu_bar.setObjectName(u"menu_bar")
+        self.menu_bar.setGeometry(QRect(0, 0, 500, 21))
+        self.file_menu = QMenu(self.menu_bar)
+        self.file_menu.setObjectName(u"file_menu")
+        self.edit_menu = QMenu(self.menu_bar)
+        self.edit_menu.setObjectName(u"edit_menu")
+        self.view_menu = QMenu(self.menu_bar)
+        self.view_menu.setObjectName(u"view_menu")
+        self.run_menu = QMenu(self.menu_bar)
+        self.run_menu.setObjectName(u"run_menu")
+        self.run_menu.setEnabled(True)
+        MainWindow.setMenuBar(self.menu_bar)
 
-        self.menuBar.addAction(self.menuFile.menuAction())
-        self.menuBar.addAction(self.menuEdit.menuAction())
-        self.menuBar.addAction(self.menuView.menuAction())
-        self.menuBar.addAction(self.menuRun.menuAction())
-        self.menuFile.addAction(self.new_action)
-        self.menuFile.addAction(self.open_action)
+        self.menu_bar.addAction(self.file_menu.menuAction())
+        self.menu_bar.addAction(self.edit_menu.menuAction())
+        self.menu_bar.addAction(self.view_menu.menuAction())
+        self.menu_bar.addAction(self.run_menu.menuAction())
+        self.file_menu.addAction(self.new_action)
+        self.file_menu.addAction(self.open_action)
+        self.run_menu.addAction(self.run_action)
 
         self.retranslateUi(MainWindow)
 
@@ -233,22 +237,23 @@ class Ui_MainWindow(object):
         self.pfile_act.setText(QCoreApplication.translate("MainWindow", u"Project File", None))
         self.new_action.setText(QCoreApplication.translate("MainWindow", u"New...", None))
         self.open_action.setText(QCoreApplication.translate("MainWindow", u"Open...", None))
+        self.run_action.setText(QCoreApplication.translate("MainWindow", u"Run", None))
         self.groupBox_3.setTitle("")
         self.open_pfile_btn.setText(QCoreApplication.translate("MainWindow", u"Open File", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Drop files here to open", None))
         ___qtreewidgetitem = self.treeWidget.headerItem()
         ___qtreewidgetitem.setText(0, QCoreApplication.translate("MainWindow", u"Project", None));
         self.groupBox_2.setTitle("")
-        self.groupBox.setTitle("")
+        self.run_box.setTitle(QCoreApplication.translate("MainWindow", u"Project file", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Data files", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Add file", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Remove", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Output file (out.csv by default)", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Cancel", None))
+        self.cancel_btn.setText(QCoreApplication.translate("MainWindow", u"Cancel", None))
         self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Run", None))
-        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
-        self.menuEdit.setTitle(QCoreApplication.translate("MainWindow", u"Edit", None))
-        self.menuView.setTitle(QCoreApplication.translate("MainWindow", u"View", None))
-        self.menuRun.setTitle(QCoreApplication.translate("MainWindow", u"Run", None))
+        self.file_menu.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+        self.edit_menu.setTitle(QCoreApplication.translate("MainWindow", u"Edit", None))
+        self.view_menu.setTitle(QCoreApplication.translate("MainWindow", u"View", None))
+        self.run_menu.setTitle(QCoreApplication.translate("MainWindow", u"Run", None))
     # retranslateUi
 
