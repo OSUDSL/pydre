bf9067b8c1534742ac175f7bb604f0c6ec044512
BuildTools <fouts.57@osu.edu>
2020-11-29 22:15:38 -0500

    Added _resize_and_center method to Window template class

--- gui/templates.py
+++ gui/templates.py
@@ -5,7 +5,7 @@
 
 import logging
 from PySide2.QtGui import QIcon
-from PySide2.QtWidgets import QMainWindow, QWidget
+from PySide2.QtWidgets import QDesktopWidget, QMainWindow, QWidget
 
 logger = logging.getLogger("PydreLogger")
 
@@ -15,22 +15,31 @@ class Window(QMainWindow):
     Parent window class that configures window UI, icon, and title if given.
     """
 
-    def __init__(self, window_ui, icon_file, title):
-        super().__init__()
-
-        # UI configurations
-        try:
-            self.ui = window_ui()
-            self.ui.setupUi(self)
-        except AttributeError:
-            logger.error("ERROR: Invalid window UI")
-        except TypeError:
-            # Allow child to inherit without using a Qt-generated UI file
-            pass
+    def __init__(self, icon_file, title, window_ui=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         # Window configurations
         self.setWindowIcon(QIcon(icon_file))
         self.setWindowTitle(title)
+
+        # UI configurations
+        if window_ui:
+            self.ui = window_ui()
+            self.ui.setupUi(self)
+
+    def _resize_and_center(self, width, height):
+        """
+        TODO
+        """
+
+        # Set window dimensions
+        self.resize(width, height)
+
+        # Center window on the screen
+        frame = self.frameGeometry()
+        screen_center = QDesktopWidget().availableGeometry().center()
+        frame.moveCenter(screen_center)
+        self.move(frame.topLeft())
 
 
 class Popup(QWidget):
@@ -38,19 +47,14 @@ class Popup(QWidget):
     Parent popup class that configures popup UI, icon, title, and geometry.
     """
 
-    def __init__(self, popup_ui, icon_file, title):
-        super().__init__()
-
-        # UI configurations
-        try:
-            self.ui = popup_ui()
-            self.ui.setupUi(self)
-        except AttributeError:
-            logger.error("ERROR: Invalid popup UI")
-        except TypeError:
-            # Allow child to inherit without using a Qt-generated UI file
-            pass
+    def __init__(self, icon_file, title, popup_ui=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         # Window configurations
         self.setWindowIcon(QIcon(icon_file))
         self.setWindowTitle(title)
+
+        # UI configurations
+        if popup_ui:
+            self.ui = popup_ui()
+            self.ui.setupUi(self)
