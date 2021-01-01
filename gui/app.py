324b15d343d3a648d4dfd06a36bd7796a9596006
BuildTools <fouts.57@osu.edu>
2021-01-01 14:55:13 -0500

    Added start method for launching the app with default config settings

--- gui/app.py
+++ gui/app.py
@@ -3,9 +3,13 @@
 # Created on: 9/17/2020
 # """
 
+from configparser import ConfigParser
 from gui.windows import MainWindow
 from PySide2.QtWidgets import QApplication
 import sys
+
+config = ConfigParser()
+config.read("./config_files/config.ini")
 
 
 class Application(QApplication):
@@ -14,16 +18,24 @@ class Application(QApplication):
     the main window.
     """
 
-    def __init__(self, window_class, ui_file, *args, **kwargs):
+    def __init__(self, window, icon_file, title, ui_file, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.app = window_class(icon_file="images/icon.png", title="Pydre",
-                                ui_file=ui_file)
+        self.app = window(icon_file=icon_file, title=title, ui_file=ui_file)
         self.app.start()
 
 
-if __name__ == '__main__':
-    window = MainWindow
-    file = "ui_files/mainwindow.ui"
-    app = Application(window, file, sys.argv)
+def start():
+    """
+    Starts the application with default config settings.
+    """
+
+    app_icon = config.get("icons", "app")
+    app_title = config.get("titles", "app")
+    app_ui = config.get("uis", "app")
+    app = Application(MainWindow, app_icon, app_title, app_ui, sys.argv)
     app.exec_()
+
+
+if __name__ == '__main__':
+    start()
