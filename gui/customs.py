c775a5210e70f2e2f0a94d7117ff1b19cb914711
BuildTools <fouts.57@osu.edu>
2021-01-14 09:39:26 -0500

    Created custom LeafWidget class for generating widgets shown in the leaves of ProjectTree widgets

--- gui/customs.py
+++ gui/customs.py
@@ -21,6 +21,80 @@ config_path = module_path + r"/config_files/config.ini"
 config.read(config_path)
 
 
+class LeafWidget(QWidget):
+    """
+    Custom leaf widget for displaying and editing project file parameters.
+    """
+
+    @staticmethod
+    def combo_box(text, items, idx=None):
+        """
+        TODO
+        """
+
+        widget = QWidget()
+        layout = QHBoxLayout()
+
+        label = QLabel("{0}:".format(text))
+        cb = QComboBox()
+
+        for item in items:
+            cb.addItem(item)
+
+        if idx is not None:
+            cb.setCurrentIndex(idx)
+
+        layout.addWidget(label)
+        layout.addWidget(cb)
+        widget.setLayout(layout)
+
+        return widget
+
+    @staticmethod
+    def spin_box(text, value=None):
+        """
+        TODO
+        """
+
+        widget = QWidget()
+        layout = QHBoxLayout()
+
+        label = QLabel("{0}:".format(text))
+        sb = QSpinBox()
+
+        if value is not None:
+            sb.setValue(value)
+
+        layout.addWidget(label)
+        layout.addWidget(sb)
+        widget.setLayout(layout)
+
+        return widget
+
+    @staticmethod
+    def line_edit(text, value=None):
+        """
+        TODO
+        """
+
+        widget = QWidget()
+        layout = QHBoxLayout()
+
+        label = QLabel("{0}:".format(text))
+        le = QLineEdit()
+
+        if value is not None:
+            le.setText(value)
+
+        le.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Preferred)
+
+        layout.addWidget(label)
+        layout.addWidget(le)
+        widget.setLayout(layout)
+
+        return widget
+
+
 class ProjectTree(QTreeWidget):
     """
     Custom tree widget for building, displaying, and editing project file trees.
@@ -59,13 +133,7 @@ class ProjectTree(QTreeWidget):
 
         # FIXME
         self.methods = metrics.metricsList
-        arguments = inspect.getfullargspec(self.methods["colMean"]).args
-        # print(self.methods)
-        # print(arguments)
-        # print(type(arguments[0]))
-        # if isinstance(arguments[0], str):
-        #     print("test1")
-        # print("{0}: {1}".format(arguments[2], get_type_hints(self.methods["colMean"])))
+
 
     def _build_metrics(self, tree, metrics_):
         """
@@ -84,35 +152,47 @@ class ProjectTree(QTreeWidget):
             le.setStyleSheet(stylesheet)
             self.setItemWidget(branch, 0, le)
 
+            types = get_type_hints(self.methods[i["function"]])
+
             leaves = [j for j in i if j != "name"]
             for k in leaves:
                 leaf = QTreeWidgetItem(branch)
 
-                widget = QWidget()
-                layout = QHBoxLayout()
-
-                label = QLabel("{0}:".format(k))
-
-                types = get_type_hints(self.methods[i["function"]])
                 if k == "function":
-                    wg = QComboBox()
-                    for method in self.methods:
-                        wg.addItem(method)
-                    idx = wg.findText(i[k])
-                    wg.setCurrentIndex(idx)
+                    idx = list(self.methods.keys()).index(i[k])
+                    widget = LeafWidget.combo_box(k, self.methods, idx)
                 elif types[k] == float:
-                    wg = QSpinBox()
-                    wg.setValue(i[k])
-                elif types[k] == str:
-                    wg = QLineEdit()
-                    wg.setText(i[k])
-                    wg.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Preferred)
-
-                layout.addWidget(label)
-                layout.addWidget(wg)
-                widget.setLayout(layout)
+                    widget = LeafWidget.spin_box(k, i[k])
+                else:
+                    widget = LeafWidget.line_edit(k, i[k])
+                    widget.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Preferred)
 
                 self.setItemWidget(leaf, 0, widget)
+
+                # widget = QWidget()
+                # layout = QHBoxLayout()
+                #
+                # label = QLabel("{0}:".format(k))
+                #
+                # if k == "function":
+                #     wg = QComboBox()
+                #     for method in self.methods:
+                #         wg.addItem(method)
+                #     idx = wg.findText(i[k])
+                #     wg.setCurrentIndex(idx)
+                # elif types[k] == float:
+                #     wg = QSpinBox()
+                #     wg.setValue(i[k])
+                # elif types[k] == str:
+                #     wg = QLineEdit()
+                #     wg.setText(i[k])
+                #     wg.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Preferred)
+                #
+                # layout.addWidget(label)
+                # layout.addWidget(wg)
+                # widget.setLayout(layout)
+                #
+                # self.setItemWidget(leaf, 0, widget)
 
     def _build_rois(self, tree, rois):
         """
