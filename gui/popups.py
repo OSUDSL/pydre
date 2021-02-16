9ff4c59bd1bdb98cc70e5212162f1804ddcc8eba
BuildTools <fouts.57@osu.edu>
2021-02-16 11:43:20 -0500

    Added self.icon_file, self.title, and self.ui_file to be natively handled by SavePopup class instead of passed as arguments

--- gui/popups.py
+++ gui/popups.py
@@ -9,6 +9,8 @@ from os import path
 
 config = Config()
 PROJECT_PATH = path.dirname(path.abspath(__file__))
+CONFIG_PATH = path.join(PROJECT_PATH, "config_files/config.ini")
+config.read(CONFIG_PATH)
 
 
 class SavePopup(Popup):
@@ -16,7 +18,22 @@ class SavePopup(Popup):
     TODO
     """
 
-    def __init__(self, icon_file, title, ui_file, *args, **kwargs):
-        super().__init__(icon_file, title, ui_file, *args, **kwargs)
+    def __init__(self, save, close, *args, **kwargs):
+        self.icon_file = path.join(PROJECT_PATH, "images/icon.png")
+        self.title = config.get("titles", "app")
+        self.ui_file = path.join(PROJECT_PATH, "ui_files/savepopup.ui")
+        super().__init__(self.icon_file, self.title, self.ui_file, *args, **kwargs)
 
+        # TODO
+        self.ui.save_btn.clicked.connect(lambda: self._save_and_close(save, close))
+        self.ui.dsave_btn.clicked.connect(lambda: print("don't save"))
+        self.ui.cancel_btn.clicked.connect(self.ui.close)
 
+    def _save_and_close(self, save, close):
+        """
+        FIXME
+        """
+
+        save()
+        self.ui.close()
+        close()
