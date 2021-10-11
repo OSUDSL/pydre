c811332881436e84b796aedb9ac54467fd0b49fb
Thomas Kerwin <kerwin.6@osu.edu>
2021-10-11 16:07:00 -0400

    Fix small bugs and typos in arriver processing

--- pydre/metrics/arriver.py
+++ pydre/metrics/arriver.py
@@ -101,7 +101,7 @@ def arrMeanLengthOfFirstOnRoadGlanceAfterAlerts(drivedata: pydre.core.DriveData)
         glance_times = []
 
         for idx in dt[alert_times].index:
-            first_gaze_on_road_index = (dt.loc[idx:, "gaze"] == "onroad").idxmin()
+            first_gaze_on_road_index = (dt.loc[idx:, "gaze"] == "onroad").idxmax()
             end_of_first_gaze_on_road_index = (dt.loc[first_gaze_on_road_index:, "gaze"] == "offroad").idxmax()
             glance_time = dt.loc[end_of_first_gaze_on_road_index].DatTime - dt.loc[first_gaze_on_road_index].DatTime
             glance_times.append(glance_time)
@@ -150,37 +150,31 @@ def arrTimeTillLaneCenterAfterLaneLoss(drivedata: pydre.core.DriveData):
 
     for dt in drivedata.data:
         if dt['SimDriverEngaged'].max() == 0:
-            return "SimDriverEngage 0"
+            return None #"SimDriverEngage 0"
         elif dt['SimDriverEngaged'].min() == 1:
-            return "SimDriverEngage 1"
-        else: 
-            
-            #dt.to_csv('temp.csv')
-            lane_loss_index = (dt.head(1)).index[0] # index when lane loss error occurs
-            lost_control_index = dt.loc[dt['SimDriverEngaged'] == 1].head(1).index[0]
-            lost_control = dt.loc[lost_control_index:]
-            regain_control_index = (lost_control.loc[lost_control['SimDriverEngaged'] == 0].head(1)).index[0]
-            after_regain_control = dt.loc[regain_control_index:]
-            center = (dt.loc[regain_control_index].LaneOffset) * 0.3
-            if (dt.loc[regain_control_index].LaneOffset < 0.01):
-                return "at center when regain control"
-            print(center)
-            #after_regain_control.to_csv('temp_regain.csv')
-            first_back_to_center = after_regain_control.loc[(after_regain_control['Lane'] == 2) & (after_regain_control['LaneOffset'] <= center)]
-            back_to_center_index = first_back_to_center.head(1).index[0]
+            return None #"SimDriverEngage 1"
+        else:
+            try:
+                lane_loss_index = (dt.head(1)).index[0] # index when lane loss error occurs
+                lost_control_index = dt.loc[dt['SimDriverEngaged'] == 1].head(1).index[0]
+                lost_control = dt.loc[lost_control_index:]
+                regain_control_index = (lost_control.loc[lost_control['SimDriverEngaged'] == 0].head(1)).index[0]
+                after_regain_control = dt.loc[regain_control_index:]
+                center = (dt.loc[regain_control_index].LaneOffset) * 0.3
+                if (dt.loc[regain_control_index].LaneOffset < 0.01):
+                    return None #"at center when regain control"
+                first_back_to_center = after_regain_control.loc[(after_regain_control['Lane'] == 2) & (after_regain_control['LaneOffset'] <= center)]
+                back_to_center_index = first_back_to_center.head(1).index[0]
 
-            print (dt.loc[back_to_center_index].DatTime)
-            print (dt.loc[regain_control_index].DatTime)
-
-            return dt.loc[back_to_center_index].DatTime - dt.loc[regain_control_index].DatTime
-            
+                return dt.loc[back_to_center_index].DatTime - dt.loc[regain_control_index].DatTime
+            except (IndexError):
+                return None
 
 
 registerMetric('arrTimeToLookBackAfterInattentive', arrTimeToLookBackAfterInattentive)
-#registerMetric('arrTimeToDisengageAfterRedAlert', arrTimeToDisengageAfterRedAlert)
 registerMetric('arrTimeTillLaneCenterAfterLaneLoss', arrTimeTillLaneCenterAfterLaneLoss)
 registerMetric('arrTimeTillLaneCenterAfterLaneLoss_crossingzero', arrTimeTillLaneCenterAfterLaneLoss_crossingzero)
-registerMetric('arrTimeToDisengageAfterRedAlert', arrTimeToDisengageAfterSlowAlert)
+registerMetric('arrTimeToDisengageAfterSlowAlert', arrTimeToDisengageAfterSlowAlert)
 registerMetric('arrTimeToDisengageAfterStopAlert', arrTimeToDisengageAfterStopAlert)
 registerMetric('arrTimeToDisengage', arrTimeToDisengage)
 registerMetric("arrTimeAfterFailureToLookAtRoad", arrTimeAfterFailureToLookAtRoad)
