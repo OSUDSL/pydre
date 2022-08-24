95b3ed0c2a016c120fd0feb5e660210327e93085
Craig Fouts <fouts.57@osu.edu>
2022-08-24 19:31:17 -0400

    Added documentation to config.py from gui-in-progress

--- gui/config.py
+++ gui/config.py
@@ -13,17 +13,23 @@ CONFIG_PATH = os.path.join(GUI_PATH, 'config_files', 'config.ini')
 
 
 class Config(ConfigParser):
-    '''Custom config class for convenience methods.
+    '''Custom configuration parser class for implementing proprietary
+    convenience methods.
 
+    Usage:
+        config = Config()
+        config.read(<configuration file path>)
     '''
 
     def __init__(self, *args, **kwargs):
+        '''Constructor.
+        '''
+
         super().__init__(*args, **kwargs)
 
     def update(self):
-        '''Updates the config file with any changes stored in the config 
-        variable.
-
+        '''Updates the application configuration file with any changes stored in 
+        the configuration variable.
         '''
 
         with open(CONFIG_PATH, 'w') as config_file:
