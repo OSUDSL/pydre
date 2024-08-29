ce221302900f96e6e63e70737c7d11f7ffc8ed1c
Thomas Kerwin <kerwin.6@osu.edu>
2024-08-29 17:03:07 -0400

    Converted json project files to toml

--- pydre_run.py
+++ pydre_run.py
@@ -7,49 +7,50 @@ import pathlib
 import glob
 import argparse
 
-parser = argparse.ArgumentParser()
-# command line arguments for project file (pf) and data file (df)
-parser.add_argument(
-    "-p", "--projectfile", type=str, help="the project file path", required=True
-)
-parser.add_argument(
-    "-d", "--datafiles", type=str, help="the data file path", nargs="+", required=True
-)
-parser.add_argument(
-    "-o",
-    "--outputfile",
-    type=str,
-    default="out.csv",
-    help="the name of the output file",
-)
-parser.add_argument(
-    "-l",
-    "--warninglevel",
-    type=str,
-    default="WARNING",
-    help="Loggging error level. DEBUG, INFO, WARNING, ERROR, and CRITICAL are allowed.",
-)
 
-args = parser.parse_args()
+def main():
+    global parser, args, p
+    parser = argparse.ArgumentParser()
+    # command line arguments for project file (pf) and data file (df)
+    parser.add_argument(
+        "-p", "--projectfile", type=str, help="the project file path", required=True
+    )
+    parser.add_argument(
+        "-d", "--datafiles", type=str, help="the data file path", nargs="+", required=True
+    )
+    parser.add_argument(
+        "-o",
+        "--outputfile",
+        type=str,
+        default="out.csv",
+        help="the name of the output file",
+    )
+    parser.add_argument(
+        "-l",
+        "--warninglevel",
+        type=str,
+        default="WARNING",
+        help="Loggging error level. DEBUG, INFO, WARNING, ERROR, and CRITICAL are allowed.",
+    )
+    args = parser.parse_args()
+    logger.remove(0)
+    try:
+        logger.add(sys.stderr, level=args.warninglevel.upper())
+    except Exception:
+        logger.add(sys.stderr, level="WARNING")
+        logger.warning("Command line log level (-l) invalid. Defaulting to WARNING")
+    if args.outputfile == "out.csv":
+        logger.warning("No output file specified. Defaulting to 'out.csv'")
+    p = pydre.project.Project(args.projectfile)
+    # test the data files
+    filelist = []
+    for fn in args.datafiles:
+        # convert relative path to absolute path
+        datapath = pathlib.Path(fn).resolve()
+        datafiles = datapath.parent.glob(datapath.name)
+        filelist.extend(datafiles)
+    p.processDatafiles(filelist, 12)
+    p.saveResults(pathlib.Path(args.outputfile))
 
-logger.remove(0)
-try:
-    logger.add(sys.stderr, level=args.warninglevel.upper())
-except Exception:
-    logger.add(sys.stderr, level="WARNING")
-    logger.warning("Command line log level (-l) invalid. Defaulting to WARNING")
-
-if args.outputfile == "out.csv":
-    logger.warning("No output file specified. Defaulting to 'out.csv'")
-
-p = pydre.project.Project(args.projectfile)
-
-# test the data files
-filelist = []
-for fn in args.datafiles:
-    # convert relative path to absolute path
-    datapath = pathlib.Path(fn).resolve()
-    datafiles = datapath.parent.glob(datapath.name)
-    filelist.extend(datafiles)
-p.processDatafiles(filelist, 12)
-p.saveResults(pathlib.Path(args.outputfile))
+if __name__ == "__main__":
+    main()
