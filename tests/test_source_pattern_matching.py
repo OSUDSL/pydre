a0b382d27bc39a72d9ec7bb45a642f2471da420d
JL <Jessica.lewis835@protonmail.com>
2026-09-10 13:08:21 -0400

    Update tests for pattern matching and datafiles to use temporary directories and files

--- tests/test_source_pattern_matching.py
+++ tests/test_source_pattern_matching.py
@@ -16,54 +16,80 @@ def test_not_supported_source(tmp_path, caplog, capsys):
     # Reads and returns captured output so far
     out, err = capsys.readouterr()
     # Assert that the specific error message was logged
-    assert ("Source specified in project definition not supported" in caplog.text or "Source specified in project definition not supported" in err)
+    assert ("Source specified in project definition not supported" in caplog.text or 
+            "Source specified in project definition not supported" in err)
 
 
 def test_pattern_matching(tmp_path):
-    toml = tmp_path / "pattern_matching.toml"
-    toml.write_text("""
+    # Create a temporary directory 
+    tmp_dir = tmp_path / "test_dir"
+    tmp_dir.mkdir()
+
+    # Create a temporary empty test data file that matches the pattern
+    tmp_test_data_file = tmp_dir / "Experimenter_43_Control_1752683477.dat"
+    tmp_test_data_file.touch()
+
+    toml = tmp_dir / "pattern_matching.toml"
+    toml.write_text(f"""
         [config]
         source = "localfilesystem"
-        baseDirectory = "C:/Users/jandj/Desktop/drive_da_files"
+        baseDirectory = "{tmp_dir.as_posix()}"
         pattern = "_Control_1752683477"
     """)
+
     # Initialize project
     project = Project(str(toml))
 
-    expected_path = Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_43_Control_1752683477.dat")
+    expected_path = Path(tmp_test_data_file)
 
     # Assert project.local_data_files contains the correct matched path
     assert  expected_path in (Path(f) for f in project.local_data_files)
 
 
 def test_pattern_matching_with_datafiles(tmp_path):
-    toml = tmp_path / "pattern_matching_and_datafiles.toml"
-    toml.write_text("""
+    # Create a temporary directory
+    tmp_dir = tmp_path / "test_dir"
+    tmp_dir.mkdir()
+
+    # Create temporary empty test data files that match the pattern and are explicitly specified in the .toml file
+    file_names = ["Experimenter_38_Test A_1750181710.dat", "Experimenter_4_Control_1734190953.dat", 
+                   "Experimenter_4_Test B_1734190306.dat", "Experimenter_4_Practice_1734189807.dat"]
+    for name in file_names:
+        (tmp_dir / name).touch()
+
+    explicit_file = (tmp_dir / "Experimenter_38_Test A_1750181710.dat").as_posix()
+
+    toml = tmp_dir / "pattern_matching_and_datafiles.toml"
+    toml.write_text(f"""
         [config]
         source = "localfilesystem"
-        baseDirectory = "C:/Users/jandj/Desktop/drive_da_files"
+        baseDirectory = "{tmp_dir.as_posix()}"
         pattern = "_4_"
-        datafiles = ["C:/Users/jandj/Desktop/drive_da_files/Experimenter_38_Test A_1750181710.dat"]
+        datafiles = ["{explicit_file}"]
     """)
+
     # Initialize project
     project = Project(str(toml))
 
-    expected_paths = {Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_38_Test A_1750181710.dat"),
-            Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_4_Control_1734190953.dat"),
-            Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_4_Test B_1734190306.dat"),
-            Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_4_Practice_1734189807.dat")}
+    expected_paths = {tmp_dir / name for name in file_names}
 
     # Assert project.local_data_files contains the correct matched path
     assert expected_paths.issubset({Path(f) for f in project.local_data_files})
 
 
 def test_datafiles_no_source(tmp_path, caplog, capsys):
-    toml = tmp_path / "datafiles_no_source.toml"
-    toml.write_text("""
+    # Create a temporary directory
+    tmp_dir = tmp_path / "test_dir"
+    tmp_dir.mkdir()
+
+    # Create a temporary empty test data file that is explicitly specified in the .toml file
+    tmp_test_data_file = (tmp_dir / "Experimenter_43_Control_1752683477.dat")
+    tmp_test_data_file.touch()
+        
+    toml = tmp_dir / "datafiles_no_source.toml"
+    toml.write_text(f"""
         [config]
-        datafiles = ["C:/Users/jandj/Desktop/drive_da_files/Experimenter_43_Control_1752683477.dat", 
-        "C:/Users/jandj/Desktop/drive_da_files/Experimenter_41_Practice_1752601959.dat", 
-        "C:/Users/jandj/Desktop/drive_da_files/Experimenter_41_Accommodation Drive_1752601503.dat"]
+        datafiles = ["{tmp_test_data_file.as_posix()}"]
     """)
 
     # Capture WARNING logs
@@ -120,26 +146,37 @@ def test_just_pattern(tmp_path, caplog, capsys):
     assert ("No source specified in project definition" in caplog.text or "No source specified in project definition" in err)
 
 
-def test_additionalFiles_patternMatching_and_datafiles(tmp_path, caplog, capsys):
-    toml = tmp_path / "patternMatching_and_datafiles.toml"
-    toml.write_text("""
+def test_additionalFiles_patternMatching_and_datafiles(tmp_path):
+    # Create a temporary directory
+    tmp_dir = tmp_path / "test_dir"
+    tmp_dir.mkdir()
+
+    # Create temporary empty test data files that match the pattern, explicitly specified in the .toml file, 
+    # and additional file paths specified through the command line
+    file_names = ["Experimenter_24_Test A_1739299126.dat", "Experimenter_38_Test A_1750181710.dat", 
+                    "Experimenter_4_Control_1734190953.dat", 
+                    "Experimenter_4_Test B_1734190306.dat",
+                    "Experimenter_4_Practice_1734189807.dat"]
+    for name in file_names:
+        (tmp_dir / name).touch()
+
+    explicit_file = (tmp_dir / "Experimenter_38_Test A_1750181710.dat").as_posix()
+
+    toml = tmp_dir / "patternMatching_and_datafiles.toml"
+    toml.write_text(f"""
         [config]
         source = "localfilesystem"
-        baseDirectory = "C:/Users/jandj/Desktop/drive_da_files"
+        baseDirectory = "{tmp_dir.as_posix()}"
         pattern = "_4_"
-        datafiles = ["C:/Users/jandj/Desktop/drive_da_files/Experimenter_38_Test A_1750181710.dat"]
+        datafiles = ["{explicit_file}"]
     """)
 
-    additional_paths = ["C:/Users/jandj/Desktop/drive_da_files/Experimenter_24_Test A_1739299126.dat"]
+    additional_paths = [f"{tmp_dir.as_posix()}/Experimenter_24_Test A_1739299126.dat"]
 
     # Intializes a project
     project = Project(str(toml), additional_paths)
 
-    expected_paths = {Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_24_Test A_1739299126.dat"),
-                Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_38_Test A_1750181710.dat"),
-                Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_4_Control_1734190953.dat"),
-                Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_4_Test B_1734190306.dat"),
-                Path("C:/Users/jandj/Desktop/drive_da_files/Experimenter_4_Practice_1734189807.dat")}
+    expected_paths = {tmp_dir / name for name in file_names}
     
     # Assert project.local_data_files contains the correct matched path, file paths explicitly specified in the .toml file, 
     # and additional file paths specified through the command line.
